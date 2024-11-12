69948a8310cd81e21295ffe7f02d6d37f7b1a065
rubendelafuente <ruben.rdelafuente@gmail.com>
2024-11-12 19:43:10 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Extension installed');
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: simplifyPage
+  });
 });
